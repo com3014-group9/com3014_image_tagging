47e241519049f82a5c14219ca9a0495aecf991dd
Arun roy <86154644+aktheroy@users.noreply.github.com>
2023-04-28 19:00:18 +0100

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,24 +1,24 @@
-import os
 from io import BytesIO
-from PIL import Image
-from unittest import TestCase
+import os
 from app import app, process_image
+import unittest
 
-class TestApp(TestCase):
+class TestApp(unittest.TestCase):
 
-    def test_process_image(self):
-        file_path = os.path.join(os.path.dirname(__file__), 'test_image.jpg')
-        with open(file_path, 'rb') as f:
-            image_bytes = f.read()
-            class_label = process_image(image_bytes)
-        expected_class_label = 'Egyptian cat'
-        self.assertEqual(class_label, expected_class_label)
+    def setUp(self):
+        self.app = app.test_client()
 
     def test_upload_file(self):
         file_path = os.path.join(os.path.dirname(__file__), 'test_image.jpg')
         with open(file_path, 'rb') as f:
             image_bytes = f.read()
-        response = self.client.post('/upload', data={'file': (BytesIO(image_bytes), 'test_image.jpg')})
-        expected_response_data = 'Egyptian cat'
+        response = self.app.post('/upload', data={'file': (BytesIO(image_bytes), 'test_image.jpg')})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), expected_response_data)
+        self.assertEqual(response.data.decode('utf-8'), 'class_label')
+
+    def test_process_image(self):
+        file_path = os.path.join(os.path.dirname(__file__), 'test_image.jpg')
+        with open(file_path, 'rb') as f:
+            image_bytes = f.read()
+        tags = process_image(image_bytes)
+        self.assertEqual(tags, 'Egyptian Cat')

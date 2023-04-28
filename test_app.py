1811498608a42291da5df86d5ff50ff04b3f2fbd
Arun roy <86154644+aktheroy@users.noreply.github.com>
2023-04-28 19:20:44 +0100

    Delete test_app.py

--- test_app.py
+++
@@ -1,9 +0,0 @@
-import os
-from app import process_image
-
-def test_process_image():
-    file_path = os.path.join(os.path.dirname(__file__), 'test_image.jpg')
-    with open(file_path, 'rb') as f:
-        image_bytes = f.read()
-        tags = process_image(image_bytes)
-    assert tags == 'Egyptian cat'

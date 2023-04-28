31b5e9dc7398883c416138888bba6934fe2ab7db
Arun roy <86154644+aktheroy@users.noreply.github.com>
2023-04-28 20:05:15 +0100

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,15 @@
-from app import process_image
+import io
+from PIL import Image
+from app import app
 
-def test_process_image():
-    with open('test_image.jpg', 'rb') as f:
-        image_bytes = f.read()
+def test_upload_file():
+    with app.test_client() as client:
+        # Load an example image for testing
+        with open('tests/example_image.jpg', 'rb') as f:
+            image_bytes = io.BytesIO(f.read())
 
-    tags = process_image(image_bytes)
-    assert isinstance(tags, str)
+        # Send a POST request to the /upload endpoint with the example image
+        response = client.post('/upload', data={'file': image_bytes})
+
+        # Check that the response contains the expected tags
+        assert b'cat' in response.data

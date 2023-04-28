3163c51860744fb7ea0729f37d53527bc8a0d722
Arun roy <86154644+aktheroy@users.noreply.github.com>
2023-04-28 17:24:04 +0100

    re write

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
-from flask import Flask, request
-from io import BytesIO
+from flask import Flask, render_template, request
 from transformers import ViTImageProcessor, ViTForImageClassification
 from PIL import Image
+import requests
+
 
 app = Flask(__name__)
 
-processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
-model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+dic = {0 : 'Cat', 1 : 'Dog'}
 
-def process_image(image_bytes):
-    image = Image.open(BytesIO(image_bytes))
-    inputs = processor(images=image, return_tensors="pt")
-    outputs = model(**inputs)
-    logits = outputs.logits
-    # model predicts one of the 1000 ImageNet classes
-    predicted_class_idx = logits.argmax(-1).item()
-    return model.config.id2label[predicted_class_idx]
+model = load_model('model.h5')
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    # Get the file from the POST request
-    file = request.files['file']
-    image_bytes = file.read()
+model.make_predict_function()
 
-    tags = process_image(image_bytes)
+def predict_label(img_path):
+	i = image.load_img(img_path, target_size=(100,100))
+	i = image.img_to_array(i)/255.0
+	i = i.reshape(1, 100,100,3)
+	p = model.predict_classes(i)
+	return dic[p[0]]
 
-    # Return a response to the client
-    return tags
 
-if __name__ == '__main__':
-    # Start the Flask application with a specified port number
-    app.run(port=5000)
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
+
+@app.route("/about")
+def about_page():
+	return "Please subscribe  Artificial Intelligence Hub..!!!"
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/" + img.filename	
+		img.save(img_path)
+
+		p = predict_label(img_path)
+
+	return render_template("index.html", prediction = p, img_path = img_path)
+
+
+if __name__ =='__main__':
+	#app.debug = True
+	app.run(debug = True)

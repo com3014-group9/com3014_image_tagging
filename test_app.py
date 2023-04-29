1bef175186aecc0c8a2a88fe77aaec4bdbffb371
Arun roy <86154644+aktheroy@users.noreply.github.com>
2023-04-29 17:38:11 +0100

    Delete test_app.py

--- test_app.py
+++
@@ -1,8 +0,0 @@
-from app import process_image
-
-def test_process_image():
-    with open('test_image.jpg', 'rb') as f:
-        image_bytes = f.read()
-
-    tags = process_image(image_bytes)
-    assert isinstance(tags, str)
